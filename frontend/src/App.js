ab160ceeb2aef3e1111d227004c614f706efcfec
k-parikh9 <106359757+k-parikh9@users.noreply.github.com>
2025-03-09 20:28:06 -0500

    checking blank pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider, useAuth } from './contexts/AuthContext';
 
 // Import other pages
 import Login from './pages/Rider/Login';
